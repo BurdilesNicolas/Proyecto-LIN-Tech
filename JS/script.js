84ad7d69911273c24b0818f6183444064ddf811a
IvanBarrera512 <ivanesc512@gmail.com>
2026-06-18 00:59:37 -0300

    Modificando Header e iniciando Main

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,16 @@
 let posicionScroll = window.scrollY;
         const header = document.querySelector(".header");
-
+        const buscador = document.querySelector(".buscador");
         window.addEventListener("scroll", function () {
 
             let scrollActual = window.scrollY;
 
             if (scrollActual < posicionScroll) {
                 header.classList.remove("header-oculto");
+                buscador.classList.remove("buscador-oculto");
             } else {
                 header.classList.add("header-oculto");
+                buscador.classList.add("buscador-oculto");
             }
 
             posicionScroll = scrollActual;
